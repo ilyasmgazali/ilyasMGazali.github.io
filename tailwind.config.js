f1f0ed3083dd08594d2e82b776eb4c458159e765
ilyasmgazali <ilyasmgazali@gmail.com>
2022-06-30 00:49:28 +0100

    set up, wrapper and progress on nav

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    borderRadius: {
+      "4xl": "2.5rem",
+    },
+    extend: {
+      colors: {
+        blue: "#007bff",
+        grayMain: "#222222",
+        indigo: "#6610f2",
+        purple: "#6f42c1",
+        pink: "#e83e8c",
+        red: "#dc3545",
+        orange: "#fd7e14",
+        yellow: "#ffc107",
+        green: "#28a745",
+        teal: "#20c997",
+        cyan: "#17a2b8",
+        white: "#fff",
+        gray: "#6c757d",
+        grayDark: "#343a40",
+        primary: "#007bff",
+        secondary: "#6c757d",
+        success: "#28a745",
+        info: "#17a2b8",
+        warning: "#ffc107",
+        danger: "#dc3545",
+        light: "#f8f9fa",
+        dark: "#343a40",
+      },
+    },
+  },
+  plugins: [],
+};
